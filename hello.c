a20b3831e6394e4ac9b93d1b3fc007f529a7e6df
张海生 <zhaisheng@rolaner.com>
2017-06-15 18:18:52 +0800

    'ss'

--- hello.c
+++ hello.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
+//定义常量
+#define USER "hisheng"
+
+
+
 int main()
 {
 	//我的第一个c程序
-	printf("%s world\n", 'hello');
+	printf("hello world\n");
+
+	int age;
+	int *ptr; 
+	age = 13;
+	printf("age 的存储大小为 %lu \n",sizeof(int));
+	printf("age 的值大小为 %d \n",age);
+
+	printf("user is %s\n", USER);
+
+	ptr = &age; //'ptr' 现在包含 'a' 的地址
+	printf("*ptr 是 %d \n", ptr);
+
+	while(age--){
+		printf("age is %s\n", age);
+	}
+
+	return 0;
 }
---
+++ t.php
@@ -0,0 +1,7 @@
+<?php
+
+
+echo "git add . \n";
+echo "git commit -m 'ss'";
+system('git add .');
+system("git commit -m 'ss'");
