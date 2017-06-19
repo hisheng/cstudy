8502e10b502772fd109ecf04dd9efe002f1527a7
张海生 <zhaisheng@rolaner.com>
2017-06-19 10:27:28 +0800

    指针做参数

---
+++ function-call-by-pointer.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+void swap(int *x , int *y)
+{
+	int tmp;
+	tmp = *x; /*保存地址x的值*/
+	*x = *y;
+	*y = tmp;
+
+	return ;
+}
+
+int main()
+{
+	/* 局部变量 定义*/
+	int a = 100 ;
+	int b = 200 ;
+
+	printf("交换前 a b 的值为 %d %d\n",a,b);
+
+	swap(a ,b);
+	printf("交换后 a b 的值为 %d %d\n",a,b);
+
+}
--- point_parm.c
+++ point_parm.c
@@ -23,5 +23,5 @@ int main(void)
 	{
 		printf("%d \n", myArray[i]);
 	}
-	return 0 ;
+	return 0
 }
