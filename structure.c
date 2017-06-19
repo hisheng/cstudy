a7927191411ebdbcaa8ff47914ff554dc27656b5
张海生 <zhaisheng@rolaner.com>
2017-06-19 11:02:40 +0800

    结构体和指针

--- structure.c
+++ structure.c
@@ -21,10 +21,10 @@ int main()
 	Book1.book_id = 1134;
 
 	/* Book2*/
-	strcpy(Book1.title , "PHP programming");
-	strcpy(Book1.author ,"php"); 
-	strcpy(Book1.subject , "php");
-	Book1.book_id = 113444;
+	strcpy(Book2.title , "PHP programming");
+	strcpy(Book2.author ,"php"); 
+	strcpy(Book2.subject , "php");
+	Book2.book_id = 113444;
 
 	 /* 输出 Book1 信息 */
    printf( "Book 1 title : %s\n", Book1.title);
