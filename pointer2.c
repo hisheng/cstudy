683beef077f1bba577f6752b7fc04fc384cf01a3
张海生 <zhaisheng@rolaner.com>
2017-06-22 11:14:24 +0800

    指针

--- pointer2.c
+++ pointer2.c
@@ -10,4 +10,12 @@ int main()
 	printf("var1 变量的地址： %d\n", &var1);
    	printf("var2 变量的地址： %x\n", &var2);
    	printf("var1 变量的地址： %d\n", &var2);
+
+   	int *ptr;
+   	int val = 1;
+
+   	ptr = &val;
+
+   	printf("ptr 变量的地址： %d\n", ptr);
+   	printf("ptr 变量的地址  的值(*ptr)： %d\n", *ptr);
 }
