fad0ba8bcae2fd08a89a9a90f53546eb1ca500bf
张海生 <zhaisheng@rolaner.com>
2017-06-29 14:25:29 +0800

    point交换数据

--- pointer/main.c
+++ pointer/main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
+void change(int *left , int *right)
+{
+	int tmp = *left;
+	*left = *right;
+	*right = tmp; 
+}
+
 int main()
 {
 	int a ;
 	int b;
+	change(&a,&b);
 	printf("num a = %d \n num b = %d ",a ,b);
 	return 0;
 }
